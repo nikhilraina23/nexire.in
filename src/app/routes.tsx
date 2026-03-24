a3e7a33951963b547de3bd533cac0435ff9197bf
Chede Nikhil <chedenihkil2@gmail.com>
2026-03-24 20:20:14 +0530

    fixed router import

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsOfService } from './pages/TermsOfService';
